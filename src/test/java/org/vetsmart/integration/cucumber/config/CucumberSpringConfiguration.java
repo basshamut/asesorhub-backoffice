bd69912af3876cfef4bcc2cb3832d50a9b538c89
Jesus Fajardo <es.jfajardobass@gmail.com>
2024-07-04 13:27:57 +0200

    Fix problem with packages that was moved

---
+++ src/test/java/org/vetsmart/integration/cucumber/config/CucumberSpringConfiguration.java
@@ -0,0 +1,11 @@
+package com.vetsmart.integration.cucumber.config;
+
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@CucumberContextConfiguration
+@SpringBootTest
+@ActiveProfiles("test")
+public class CucumberSpringConfiguration {
+}
