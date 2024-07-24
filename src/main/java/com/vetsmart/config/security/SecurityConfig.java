512232f50c3bf75017ccc6b051f66a86bbf367d0
Jesus Fajardo <es.jfajardobass@gmail.com>
2024-07-24 14:19:43 +0200

    PoC Keycloak

--- src/main/java/com/vetsmart/config/security/SecurityConfig.java
+++ src/main/java/com/vetsmart/config/security/SecurityConfig.java
@@ -1,76 +1,36 @@
 package com.vetsmart.config.security;
 
-import com.vetsmart.config.security.jwt.JWTAuthorizationFilter;
-import com.vetsmart.config.security.provider.CustomAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import java.util.Collections;
-
-import static com.vetsmart.utils.Constants.API_VERSION_PATH;
-import static com.vetsmart.utils.Constants.LOGIN_PATH;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    @Autowired
-    private CustomAuthenticationProvider customAuthenticationProvider;
-
-    private final String[] WHITE_LIST = {
-            "/**",
-            "/swagger*/**",
-            "/v3/api-docs/**",
-            "/console/**",
-            "/error",
-            API_VERSION_PATH + LOGIN_PATH
-    };
-
-    // Configuring HttpSecurity
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> {
-                    try {
-                        // Create MvcRequestMatchers for MVC endpoints
-                        for (String pattern : WHITE_LIST) {
-                            auth.requestMatchers(new MvcRequestMatcher(introspector, pattern)).permitAll();
-                        }
-                    } catch (Exception e) {
-                        throw new RuntimeException("Failed to configure MVC request matchers", e);
-                    }
-                    auth.anyRequest().authenticated();
-                })
-                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .addFilter(getJwtAuthorizationFilter())
-        ;
-        http.headers().frameOptions().disable(); // Allow H2 console (if needed for development)
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers(
+                        "/unauthenticated",
+                        "/oauth2/**",
+                        "/login/**",
+                        "/swagger*/**",
+                        "/v3/api-docs/**",
+                        "/console/**",
+                        "/swagger*/**",
+                        "/v3/api-docs/**",
+                        "/error"
+                ).permitAll();
+        http
+                .authorizeHttpRequests(authz -> authz.anyRequest().authenticated())
+                .addFilterAfter(new JwtKeycloakSecurityFilter(), BasicAuthenticationFilter.class);
+
+
         return http.build();
     }
-
-    @Bean
-    public AuthenticationManager authenticationManager() {
-        return new ProviderManager(Collections.singletonList(customAuthenticationProvider));
-    }
-
-    public JWTAuthorizationFilter getJwtAuthorizationFilter() {
-        return new JWTAuthorizationFilter(authenticationManager());
-    }
-
 }
