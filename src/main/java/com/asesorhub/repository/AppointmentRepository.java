7cda733ca808590b540462fa46d165e0c384c5e4
Jesus Fajardo <es.jfajardobass@gmail.com>
2025-01-21 12:10:58 +0100

    Remove unnecessary entities

--- src/main/java/com/asesorhub/repository/AppointmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asesorhub.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
-}
-
